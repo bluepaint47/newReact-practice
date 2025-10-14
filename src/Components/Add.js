f7afffeefa08e7facbe454f514250350f2af079e
GF Blue <gfblue47@gmail.com>
2025-10-14 18:59:33 +0500

    hello

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -5,6 +5,9 @@ function Add() {
      <>
      <div className="hp">
      <h1 >hello to add two numberss</h1>
+     <h1> hello to my first react js
+        
+     </h1>
      </div>
     
      </>
