e51f00b50ed573c991a78575ea39ef82ebfba62f
GF Blue <gfblue47@gmail.com>
2025-10-14 18:51:58 +0500

    "new updates"

---
+++ src/Components/Add.js
@@ -0,0 +1,14 @@
+
+import './add.css';
+function Add() {
+    return (
+     <>
+     <div className="hp">
+     <h1 >hello to add two numberss</h1>
+     </div>
+    
+     </>
+    );
+  }
+  
+  export default Add;
